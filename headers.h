9323ee707762523693c92879943252feb92ebf83
IGuessICode <111307241+IGuessICode@users.noreply.github.com>
2022-10-02 09:29:11 +0200

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,10 @@
+// Header indicators
+#ifndef HEADERS.H
+#define HEADERS.H
+
+// Function headers
+#include "wificollector_quit.h"
+#include "wificollector_collect.h"
+#include "wificollector_display.h" // includes display and display_all
+
+#endif
