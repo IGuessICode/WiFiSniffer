f8aba6c45b82921920699f834f17a4586512ec4d
IGuessICode <111307241+IGuessICode@users.noreply.github.com>
2022-10-02 11:25:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,39 +9,52 @@
 #include "headers.h"
 
 // CONSTANTS
+#define MAX_LENGTH_INPUT 2
 #define MAX_LENGTH_ARRAY 200
 #define MAX_LENGTH_STR 81 // 80 char + \0
 
 // main
 int main() {
-	// Menu display
-	char option[2];
-	
-	printf("[2022] SUCEM S.L. Wifi Collector\n\n [ 1]wificollector_quit\n [ 2]wificollector_collect\n [ 3]wificollector_show_data_one_network\n [ 4]wificollector_select_best\n [ 5]wificollector_select_worst\n [ 6]wificollector_delete_net\n [ 7]wificollector_sort\n [ 8]wificollector_export\n [ 9]wificollector_import\n [10]wificollector_display\n [11]wificollector_display_all\n\n");
-	do {
-		printf("Option: \n");
-		scanf("%s", option);
-		if (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "6" && option != "7" && option != "8" && option != "9" && option != "10" && option != "11") {
-			printf("Please introduce a valid option.");
-		}
 
-	}while (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "6" && option != "7" && option != "8" && option != "9" && option != "10" && option != "11");
+    char input[MAX_LENGTH_INPUT];
+    int option;
 
-	switch (option) {
-		case "1": wificollector_quit; break;
-		case "2": wificollector_collect(); break;
-		case "10": wificollector_display(); break;
-		case "11": wificollector_display_all(); break;
-		default: break;
-	}
-	
-	// PROBLEM: Repeat the menu once again after option is executed.
+    do {
+        printf("\n [2022] SUCEM S.L. Wifi Collector \n \n");
 
-	return 0;
+        printf("[ 1] wificollector_quit \n");
+        printf("[ 2] wificollector_collect \n");
+        printf("[ 3] wificollector_show_data_one_network \n");
+        printf("[ 4] wificollector_select_best \n");
+        printf("[ 5] wificollector_select_worst \n");
+        printf("[ 6] wificollector_delete_net \n");
+        printf("[ 7] wificollector_sort \n");
+        printf("[ 8] wificollector_export \n");
+        printf("[ 9] wificollector_import \n");
+        printf("[10] wificollector_display \n");
+        printf("[11] wificollector_display_all \n \n");
+
+        do {
+			printf("Option: ");
+			fgets(input, STR, stdin);
+			option = atoi(input);
+			if (option < 1 || option > 11) {
+				printf("Please introduce a valid option.");
+			}
+	    } while (option < 1 || option > 11);
+        reset(input);
+
+        switch (option) {
+
+            case 1: wificollector_quit(); break;
+            case 2: //wificollector_collect(); break;
+            case 10: //wificollector_display(); break;
+            case 11: //wificollector_display_all(); break;
+            default:
+                break;
+        }
+
+    } while (quit[0] != 'y' && quit[0] != 'Y');
+
+    return 0;
 }
-
-// functions -  Do they need to be declared before they are used or smt?
-//	void wificollector_quit()
-//	void wificollector_collect()
-//	void wificollector_display()
-//	void wificollector_display_all()
