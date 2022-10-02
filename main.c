124e3602df0234eb16912922eb281cf430fcade1
IGuessICode <111307241+IGuessICode@users.noreply.github.com>
2022-10-02 09:22:33 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+// Submission 1 ver.
+// Litterbox coding
+
+// LIBRARIES
+#include <stdio.h>
+#include <stdlib.h> // lib for I/O
+
+// HEADER FILES
+#include "wificollector_quit.h"
+#include "wificollector_collect.h"
+#include "wificollector_display.h" // includes display and display_all
+
+// CONSTANTS
+#define MAX_LENGTH_ARRAY 200
+#define MAX_LENGTH_STR 81 // 80 char + \0
+
+// main
+int main() {
+	// Menu display
+	char option[2];
+	
+	printf("[2022] SUCEM S.L. Wifi Collector\n\n [ 1]wificollector_quit\n [ 2]wificollector_collect\n [ 3]wificollector_show_data_one_network\n [ 4]wificollector_select_best\n [ 5]wificollector_select_worst\n [ 6]wificollector_delete_net\n [ 7]wificollector_sort\n [ 8]wificollector_export\n [ 9]wificollector_import\n [10]wificollector_display\n [11]wificollector_display_all\n\n");
+	do {
+		printf("Option: \n");
+		scanf("%s", option);
+		if (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "6" && option != "7" && option != "8" && option != "9" && option != "10" && option != "11") {
+			printf("Please introduce a valid option.");
+		}
+
+	}while (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "6" && option != "7" && option != "8" && option != "9" && option != "10" && option != "11");
+
+	switch (option) {
+		case "1": wificollector_quit; break;
+		case "2": wificollector_collect(); break;
+		case "10": wificollector_display(); break;
+		case "11": wificollector_display_all(); break;
+		default: break;
+	}
+	
+	// PROBLEM: Repeat the menu once again after option is executed.
+
+	return 0;
+}
+
+// functions
+//	void wificollector_quit()
+//	void wificollector_collect()
+//	void wificollector_display()
+//	void wificollector_display_all()
