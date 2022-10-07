52902af28143b7583982edd6d8b33ec18c2d94ec
isabelschweim <93743300+isabelschweim@users.noreply.github.com>
2022-10-07 23:20:45 +0200

    Add files via upload

---
+++ display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#define MAX 200 //arrays
+#define STR 80 //strings
+
+int print(int n);
+void wificollector_display(void);
+void wificollector_display_all(void);
+
+#endif //DISPLAY_H
